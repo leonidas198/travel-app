05e693bdf0e4e5a8f682ca73372eb34585bdb11c
[quique] <[enriquechavez1982@hotmail.com]>
2025-06-30 10:43:42 -0300

    site add

--- scripts/generate-sitemap.cjs
+++ scripts/generate-sitemap.cjs
@@ -13,13 +13,14 @@ let urls = [
 ];
 
 let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">\n`;
 
 // Generar URLs estáticas
 locales.forEach((lang) => {
   urls.forEach((url) => {
     sitemap += `  <url>\n`;
-    sitemap += `    <loc>${baseUrl}/${lang}${url}</loc>\n`;
+    sitemap += `    <loc>${baseUrl}/${lang}${url}/</loc>\n`;
     sitemap += `    <changefreq>weekly</changefreq>\n`;
     sitemap += `    <priority>0.9</priority>\n`;
     sitemap += `  </url>\n`;
@@ -30,7 +31,7 @@ locales.forEach((lang) => {
 destinations.forEach(({ slug }) => {
   locales.forEach((lang) => {
     sitemap += `  <url>\n`;
-    sitemap += `    <loc>${baseUrl}/${lang}/destinos/${slug}</loc>\n`;
+    sitemap += `    <loc>${baseUrl}/${lang}/destinos/${slug}/</loc>\n`;
     sitemap += `    <changefreq>monthly</changefreq>\n`;
     sitemap += `    <priority>0.8</priority>\n`;
     sitemap += `  </url>\n`;
@@ -42,3 +43,4 @@ sitemap += `</urlset>\n`;
 // Guardar archivo
 fs.writeFileSync(path.join(__dirname, "../public/sitemap.xml"), sitemap);
 console.log("✅ Sitemap generado correctamente.");
+
